4138ba119dbe6af5150625b906b4bd7f06fe1df8
Wilson Whitaker <wilson.whitaker@cfacorp.com>
2021-08-27 11:10:14 -0400

    Remove VLAN 20
    No longer in use

--- udpbroadcastrelay.sh
+++ udpbroadcastrelay.sh
@@ -8,15 +8,15 @@
 #!/bin/sh
 rc_start() {
 
-    /usr/bin/nohup /usr/local/sbin/udpbroadcastrelay --id 1 --port 5353 --dev igb1 --dev igb2.20 --multicast 224.0.0.251 -s 1.1.1.1 -f > /dev/null
-    /usr/bin/nohup /usr/local/sbin/udpbroadcastrelay --id 2 --port 1900 --dev igb1 --dev igb2.20 --multicast 239.255.255.250 -s 1.1.1.1 -f > /dev/null
-    /usr/bin/nohup /usr/local/sbin/udpbroadcastrelay --id 3 --port 6969 --dev igb1 --dev igb2.20 --multicast 239.255.255.250 -f > /dev/null
+    # Interface 2
+    /usr/bin/nohup /usr/local/sbin/udpbroadcastrelay --id 1 --port 5353 --dev igb1 --dev igb2 --multicast 224.0.0.251 -s 1.1.1.1 -f > /dev/null
+    /usr/bin/nohup /usr/local/sbin/udpbroadcastrelay --id 2 --port 1900 --dev igb1 --dev igb2 --multicast 239.255.255.250 -s 1.1.1.1 -f > /dev/null
+    /usr/bin/nohup /usr/local/sbin/udpbroadcastrelay --id 3 --port 6969 --dev igb1 --dev igb2 --multicast 239.255.255.250 -f > /dev/null
 
-
-    /usr/bin/nohup /usr/local/sbin/udpbroadcastrelay --id 4 --port 5353 --dev igb1 --dev igb2 --multicast 224.0.0.251 -s 1.1.1.1 -f > /dev/null
-    /usr/bin/nohup /usr/local/sbin/udpbroadcastrelay --id 5 --port 1900 --dev igb1 --dev igb2 --multicast 239.255.255.250 -s 1.1.1.1 -f > /dev/null
-    /usr/bin/nohup /usr/local/sbin/udpbroadcastrelay --id 6 --port 6969 --dev igb1 --dev igb2 --multicast 239.255.255.250 -f > /dev/null
-
+    # Interface 2, VLAN 20
+#     /usr/bin/nohup /usr/local/sbin/udpbroadcastrelay --id 4 --port 5353 --dev igb1 --dev igb2.20 --multicast 224.0.0.251 -s 1.1.1.1 -f > /dev/null
+#     /usr/bin/nohup /usr/local/sbin/udpbroadcastrelay --id 5 --port 1900 --dev igb1 --dev igb2.20 --multicast 239.255.255.250 -s 1.1.1.1 -f > /dev/null
+#     /usr/bin/nohup /usr/local/sbin/udpbroadcastrelay --id 6 --port 6969 --dev igb1 --dev igb2.20 --multicast 239.255.255.250 -f > /dev/null
 }
 
 rc_stop() {
